e3ea0b12100ab2ee6de087d7c56fa6ccf8643916
Andreia Silva <apsilva3@gmail.com>
2025-05-20 18:50:03 -0300

    gitActions

---
+++ src/app/gitActions.tsx
@@ -0,0 +1,14 @@
+/*Arquivo para executar o gitActions*/
+import React from "react";
+
+const PaginaSimples: React.FC = () => {
+  return (
+    <div>
+      <h1>Olá, mundo!</h1>
+      <p>Esta é uma página TSX simples.</p>
+    </div>
+  );
+};
+
+export default PaginaSimples;
+
